4dd0e986b32fcf24800dba92876980b0304985b1
Chinmoy <chinmoy.debnath11@gmail.com>
2017-03-01 20:52:00 +0530

    initial commit

---
+++ src/stores/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/index';
+
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+export default function configureStore(initialState) {
+  return createStoreWithMiddlware(rootReducer, initialState);
+}
